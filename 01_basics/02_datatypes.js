e245b6fc4a6afab7e376e8a7e14dbab579faa4c2
Naman Srivastava <srivastavanaman125@gmail.com>
2025-02-25 13:22:39 +0000

    learning about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // e are using nodejs , not browser
+
+console.log(3+3)
+// code readeablity should be high 
+console.log("Naman")
+
+let name = "Aman"
+let age = 21
+let state;
+let isloggedIn = false
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof null)
